d75197dd1d4eb80455f0b29d4ca546b16723c52c
Johnathan Bevers <beversjohnathan7@gmail.com>
2021-08-07 10:02:00 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Johnathan Bevers',
     description:
       'Johnathan Bevers is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://johnathanbevers.dev', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'JohnathanBevers',
+        short_name: 'JohnathanBevers',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
