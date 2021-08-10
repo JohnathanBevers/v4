5c550310c36a189314b2f3e8e70997fb83c5b3dd
Johnathan Bevers <beversjohnathan7@gmail.com>
2021-08-09 22:46:33 -0500

    Updating footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/JohnathanBevers">
+        <a href="https://github.com/bchiang7/v4">
           <div>Designed &amp; Built by brittanychiang.com</div>
 
           {githubInfo.stars && githubInfo.forks && (
