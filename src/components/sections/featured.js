62f47cbd245c692a259d2ecb05ef8b497290566b
Johnathan Bevers <beversjohnathan7@gmail.com>
2021-08-25 18:48:11 -0500

    Updating Projects

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -337,7 +337,7 @@ const Featured = () => {
   return (
     <section id="projects">
       <h2 className="numbered-heading" ref={revealTitle}>
-        Some Things I’ve Built
+        Some Projects I Have Developed
       </h2>
 
       <StyledProjectsGrid>
